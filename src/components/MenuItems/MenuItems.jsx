d274afe4da6eeba9b4426b5553d6742e33894ee0
Maggie Jenkins <adair.jenkins@gmail.com>
2022-03-31 14:21:37 -0500

    functional add and remove buttons on menu

--- src/components/MenuItems/MenuItems.jsx
+++ src/components/MenuItems/MenuItems.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 // import axios from 'axios'; /probs irrelevant, but could need in theory for eventual delete button
+import {useDispatch} from 'react-redux';
+
 
 function MenuItems({ pizza }) {
+    const dispatch = useDispatch();
+    
     console.log(pizza);
+
+    const addToCart = () => {
+        console.log('add to cart:', pizza.name);
+        dispatch({type: 'ADD_PIZZA', payload: pizza})
+    }
+
+    const removeFromCart = () => {
+        console.log('remove from cart:', pizza.name, pizza.id);
+        dispatch({type: 'REMOVE_PIZZA', payload: pizza})
+    }
 
     return (
         <>
@@ -14,6 +28,8 @@ function MenuItems({ pizza }) {
                     <p>{pizza.name} <span>{pizza.price}</span></p>
                     <p>{pizza.description}</p>
             </div>
+            <button onClick={addToCart}>ADD</button>
+            <button onClick={removeFromCart}>REMOVE</button>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -8,11 +8,37 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
-const orderReducer = (state = [], action) => {
-    if (action.type === 'ADD_ORDER') {
-        return [...state, action.payload]
+const order = {
+    customer_name: '',
+    street_address: '',
+    city: '',
+    zip: '',
+    total: '',
+    type: '',
+    pizzas: []
+}
+
+const orderReducer = (state = order, action) => {
+    switch (action.type) {
+        
+        case 'ADD_PIZZA':
+            state.pizzas = [...state.pizzas, action.payload];
+            return state;
+        
+        case 'REMOVE_PIZZA':
+            return state.pizzas.filter((pizza) => pizza.id !== action.payload.id);
+        
+        case 'ADD_ORDER':
+            state.customer = action.payload.customer;
+            state.street_address = action.payload.street_address;
+            state.city = action.payload.city;
+            state.zip = action.payload.zip;
+            state.total = action.payload.total;
+            state.type = action.payload.type;
+        
+        default:
+            return state;
     }
-    return state;
 }
 
 const menuReducer = (state = [], action) => {
