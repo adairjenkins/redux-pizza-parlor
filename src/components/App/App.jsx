58147a586fd061853c633a5db941c5ec4519c296
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-03-31 11:06:56 -0500

    adds get route and routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import Header from '../Header/Header';
 import MenuList from '../MenuList/MenuList';
 import Checkout from '../Checkout/Checkout';
+import OrderForm from '../OrderForm/OrderForm';
+import { useDispatch } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
 
-  return (
-    <div className='App'>
-      < Header/>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
+  useEffect(() => {
+    getPizzas();
+  }, []);
 
+  const dispatch = useDispatch();
+
+  const getPizzas = () => {
+    //selects * from pizza table in database:
+    axios.get('/api/pizza')
+      .then(response => {
+        dispatch({ type: 'GET_MENU', payload: response.data })
+      })
+      .catch((error) => {
+        console.log('error getting pizzas: ', error);
+      })
+  }
+
+
+  return (
+    <Router>
+      <Route path='/'>
+        <div className='App'>
+          < Header />
+          <img src='images/pizza_photo.png' />
+          <p>Pizza is great.</p>
+      </Route>
+
+      <Route path='/menu'>
+      {/* Step 1: Select your Pizza */}
       <MenuList />
-    
-      <Checkout/>
+      </Route>
+
+      <Route path='/form'>     
+      {/* Step 2: Customer Information */}
+      <OrderForm />
+      </Route> 
+
+      <Route path='/checkout'>
+      {/* Step 3: Checkout */}
+      <Checkout />
+      </Route>
+      
     </div>
+    </Router >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const orderReducer = (state = [], action) => {
+    if (action.type === 'ADD_ORDER') {
+        return [...state, action.payload]
+    }
+    return state;
+}
+
+const menuReducer = (state = [], action) => {
+    if (action.type === 'GET_MENU') {
+        return action.payload;
+    }
+    return state;
+}
+
+const store = createStore(
+    combineReducers({
+        orderReducer,
+        menuReducer
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
