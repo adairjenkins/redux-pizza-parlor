7e17d0bd1b7981c49621af2bc67b9c8ebc2277f8
Maggie Jenkins <adair.jenkins@gmail.com>
2022-04-01 09:37:29 -0500

    toggle between add and remove unless a pizza is re-added

--- src/components/MenuItems/MenuItems.jsx
+++ src/components/MenuItems/MenuItems.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 // import axios from 'axios'; /probs irrelevant, but could need in theory for eventual delete button
 import {useDispatch} from 'react-redux';
+import {useState} from 'react';
 
 
 function MenuItems({ pizza }) {
     const dispatch = useDispatch();
+
+    const [btnStatus, setBtnStatus] = useState(true);
+
+    const handleBtnToggle = () => {
+        setBtnStatus(!btnStatus);
+    }
     
     console.log(pizza);
 
     const addToCart = () => {
         console.log('add to cart:', pizza.name);
         dispatch({type: 'ADD_PIZZA', payload: pizza})
+        handleBtnToggle();
     }
 
     const removeFromCart = () => {
         console.log('remove from cart:', pizza.name);
         dispatch({type: 'REMOVE_PIZZA', payload: pizza})
+        handleBtnToggle();
     }
 
     return (
@@ -28,8 +37,8 @@ function MenuItems({ pizza }) {
                     <p>{pizza.name} <span>{pizza.price}</span></p>
                     <p>{pizza.description}</p>
             </div>
-            <button onClick={addToCart}>ADD</button>
-            <button onClick={removeFromCart}>REMOVE</button>
+            {btnStatus ? <button onClick={addToCart}>ADD</button> 
+                       : <button onClick={removeFromCart}>REMOVE</button>}
         </>
     )
 }
